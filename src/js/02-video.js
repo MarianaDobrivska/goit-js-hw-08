dda5ab5cf31267d6a81529f641c94137629b7e26
MarianaDobrivska <dobrivska.marry@gmail.com>
2022-11-09 01:46:08 +0200

    update 2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const localKey = 'videoplayer-current-time';
+const currenTime = localStorage.getItem(localKey);
+const player = new Player(iframe);
+
+function getSeconds({ seconds }) {
+  localStorage.setItem(localKey, seconds);
+}
+
+player.on('timeupdate', throttle(getSeconds, 1000));
+
+player
+  .setCurrentTime(currenTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
